d8702e57225248e21796284d67e6bfa779ff41e7
Darren Chaddock <dchaddoc@ucalgary.ca>
2023-05-31 15:45:20 +0000

    adjustments for one worker

--- python/themis_imager_readfile/__init__.py
+++ python/themis_imager_readfile/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "1.1.4"
+__version__ = "1.2.0"
 
 from ._themis import read
--- python/themis_imager_readfile/_themis.py
+++ python/themis_imager_readfile/_themis.py
@@ -1,10 +1,7 @@
-# Author: Lukas Vollmerhaus
-# Date: 2020-10-16
-
 import gzip
 import bz2
-import numpy as np
 import signal
+import numpy as np
 from multiprocessing import Pool
 
 # globals
@@ -35,16 +32,33 @@ def read(file_list, workers=1):
     if isinstance(file_list, str):
         file_list = [file_list]
 
-    # call readfile function, run each iteration with a single input file from file_list
-    # NOTE: structure of data - data[file][metadata dictionary lists = 1, images = 0][frame]
-    data = []
-    try:
-        data = pool.map(__themis_readfile_worker, file_list)
-    except KeyboardInterrupt:
-        pool.terminate()  # gracefully kill children
-        return np.empty((0, 0, 0), dtype=THEMIS_DT), [], []
+    # check workers
+    if (workers > 1):
+        try:
+            # set up process pool (ignore SIGINT before spawning pool so child processes inherit SIGINT handler)
+            original_sigint_handler = signal.signal(signal.SIGINT, signal.SIG_IGN)
+            pool = Pool(processes=workers)
+            signal.signal(signal.SIGINT, original_sigint_handler)  # restore SIGINT handler
+        except ValueError:
+            # likely the read call is being used within a context that doesn't support the usage
+            # of signals in this way, proceed without it
+            pool = Pool(processes=workers)
+
+        # call readfile function, run each iteration with a single input file from file_list
+        # NOTE: structure of data - data[file][metadata dictionary lists = 1, images = 0][frame]
+        data = []
+        try:
+            data = pool.map(__themis_readfile_worker, file_list)
+        except KeyboardInterrupt:
+            pool.terminate()  # gracefully kill children
+            return np.empty((0, 0, 0), dtype=THEMIS_DT), [], []
+        else:
+            pool.close()
     else:
-        pool.close()
+        # don't bother using multiprocessing with one worker, just call the worker function directly
+        data = []
+        for f in file_list:
+            data.append(__themis_readfile_worker(f))
 
     # derive number of frames to prepare for
     total_num_frames = 0
